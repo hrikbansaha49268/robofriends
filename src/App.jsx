6072bba8e3955275515b3336f418e11c1b5e5644
hrikbansaha49268 <hrikbansaha@gmail.com>
2022-01-12 17:24:43 +0530

    Finished building

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './components/CardList';
+import Scroll from './components/Scroll';
 import Searchbox from './components/Searchbox';
-import { robots } from './data/robots';
 import './style.css';
 
 
@@ -10,26 +10,38 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
     };
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response =>
+            response.json()).then(users => this.setState({ robots: users }));
+    }
 
     onSearch = (event) => {
         this.setState({ searchField: event.target.value });
     };
 
+
     render() {
 
         const filteredRobos = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
+
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc">Loading...</h1>;
+        }
 
         return (
             <div className="tc" >
                 <h1 className="">Robo Friends</h1>
                 <Searchbox searchbox={this.onSearch} />
-                <CardList robots={filteredRobos} />
+                <Scroll>
+                    <CardList robots={filteredRobos} />
+                </Scroll>
             </div>
         )
     };
